b9bbcd865bb153a1c84a29fa32c9bbb6824b8fb9
DiyorbekBoltayev <boltayevdiyorbek017@gmail.com>
2022-10-24 21:46:17 +0500

    Migrations

---
+++ database/migrations/2022_10_24_163033_create_time_controls_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('time_controls', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('object_id')->constrained()->onDelete('cascade');
+            $table->dateTime('start');
+            $table->dateTime('end');
+            $table->tinyInteger('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('time_controls');
+    }
+};
